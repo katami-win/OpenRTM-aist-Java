39e53a563f637fa820c4effd04ba27d03239b1dd
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-05-13 16:31:20 +0900

    [compat] fixed bugs

--- jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/RTObjectStateMachine.java
+++ jp.go.aist.rtm.RTC/src/jp/go/aist/rtm/RTC/RTObjectStateMachine.java
@@ -387,6 +387,10 @@ public class RTObjectStateMachine {
      *
      */
     public void onExecute(final StateHolder<LifeCycleState> st){
+        if (isNextState(LifeCycleState.ERROR_STATE))
+        {
+            return; 
+        }
         if (!m_dfc) { 
             return; 
         }
@@ -400,6 +404,10 @@ public class RTObjectStateMachine {
      *
      */
     public void onStateUpdate(final StateHolder<LifeCycleState> st){
+        if (isNextState(LifeCycleState.ERROR_STATE))
+        {
+            return; 
+        }
         // call Servant
         if (m_rtobjPtr != null) {
             if (m_rtobjPtr.on_state_update(m_id) != ReturnCode_t.RTC_OK) {
